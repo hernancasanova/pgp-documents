d8fe1a2ffc787bc4a7d47cba4fee5d01f1d6ddd2
Hernán Casanova <hernan.casanova.silva@outlook.com>
2023-10-17 17:30:04 -0300

    inicio del proyecto

---
+++ src/test/java/com/pgp/documents/PgpDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgp.documents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgpDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
